152b358ab8a6c4931b76bc3f7473bc89d576e171
Karl Seguin <karl@openmymind.net>
2012-02-09 12:09:14 +0800

    test for delete

---
+++ Parse.Tests/FilesTests/DeleteTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Parse.Tests.FilesTests
+{
+   public class DeleteTests : BaseFixture
+   {
+      [Test]
+      public void SendsADeleteRequest()
+      {
+         Server.Stub(new ApiExpectation { Method = "DELETE", Url = "/1/files/sand.txt", Request = "", Response = "" });
+         new Driver().Files.Delete("sand.txt", SetIfSuccess);
+         WaitOne();
+      }
+   }
+}
